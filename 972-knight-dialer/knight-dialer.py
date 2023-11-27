1b6e7733eee057058a9d251a7c0fc051ea16abdb
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2023-11-27 08:32:17 -0800

    Time: 3213 ms (26.96%) | Memory: 47.6 MB (27.49%) - LeetSync

--- 972-knight-dialer/knight-dialer.py
+++ 972-knight-dialer/knight-dialer.py
@@ -1,19 +1,23 @@
 class Solution:
+    
     def knightDialer(self, n: int) -> int:
+        directions = [[2,1], [1,2], [-2,1], [-2,-1], [-1,-2], [-1, 2], [2,-1], [1,-2]]
         @lru_cache(None)
+        
+        
         def helper(n, row, col):
             if n==0:
                 return 1
-            ans = 0 
-            for r, c in [[2,1], [1,2], [-2,1], [-1,2], [2,-1], [1,-2], [-2,-1], [-1,-2]]:
+            ans = 0
+            for r, c in directions:
                 nr = row+r
                 nc = col+c
-                if (-1<nr<3 and -1<nc<3) or (nr == 3 and nc==1):
+                if (0<=nr<3 and 0<=nc<3) or (nr==3 and nc==1):
                     ans+=helper(n-1, nr, nc)
             return ans%(10**9+7)
         
         ans = helper(n-1, 3,1)
         for i in range(3):
             for j in range(3):
-                ans+=helper(n-1, i, j)
+                ans+=helper(n-1,i,j)
         return ans%(10**9+7)
