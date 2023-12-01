25177f30a15bab66b9deaf12e2189abbe99f8034
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2023-12-01 08:18:47 -0800

    Time: 58 ms (72.10%) | Memory: 16.5 MB (6.48%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,18 +1,31 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if s=="":
-            return 0
-        ans = float('-inf')
-        l = 0
-        subs = []
-        nums = list(s)
-        for r in range(len(nums)):
-            subs += nums[r]
-            if len(set(subs))==r-l+1:
-                ans = max(ans, r-l+1)
-            else:
-                while len(set(subs))!=r-l+1:
-                    subs.pop(0)
-                    l+=1
-                ans = max(ans, r-l+1)
+        # if s=="":
+        #     return 0
+        # ans = float('-inf')
+        # l = 0
+        # subs = []
+        # nums = list(s)
+        # for r in range(len(nums)):
+        #     subs += nums[r]
+        #     if len(set(subs))==r-l+1:
+        #         ans = max(ans, r-l+1)
+        #     else:
+        #         while len(set(subs))!=r-l+1:
+        #             subs.pop(0)
+        #             l+=1
+        #         ans = max(ans, r-l+1)
+        # return ans
+
+        ans = 0
+        subs = set()
+        l =0
+        
+
+        for r in range(len(s)):
+            while s[r] in subs:
+                subs.remove(s[l])
+                l+=1
+            subs.add(s[r])
+            ans = max(ans, len(subs))
         return ans
