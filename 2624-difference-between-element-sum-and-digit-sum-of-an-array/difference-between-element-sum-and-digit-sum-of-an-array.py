5ff8451065ffd5e23bf93a65188900deff424f89
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2023-12-03 09:14:13 -0800

    Time: 127 ms (50.95%) | Memory: 16.5 MB (55.00%) - LeetSync

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.py
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.py
@@ -3,6 +3,11 @@ class Solution:
         ele, dig = 0,0
         for x in nums:
             ele+=x
-            for i in str(x):
-                dig+=int(i)
+            dig += self.digit_sum(x)
         return ele-dig
+    def digit_sum(self, n):
+        result = 0
+        while n:
+            result += int(n%10)
+            n //= 10
+        return result
