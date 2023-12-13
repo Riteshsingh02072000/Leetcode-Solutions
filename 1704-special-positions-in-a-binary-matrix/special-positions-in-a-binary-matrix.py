ec5c979ac3a5828b425d7184cacf5e52457de7c4
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2023-12-13 09:55:10 -0800

    Time: 154 ms (62.69%) | Memory: 17.6 MB (5.50%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
@@ -2,6 +2,7 @@ class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
         row =  collections.defaultdict(int)
         col =  collections.defaultdict(int)
+        seen = set()
         count = 0
 
         for i in range(len(mat)):
@@ -9,10 +10,14 @@ class Solution:
                 if mat[i][j] == 1:
                     row[i] += 1
                     col[j] += 1
-                
-        for i in range(len(mat)):
-            for j in range(len(mat[0])):
-                if mat[i][j] == 1:
-                    if row[i]==1 and col[j]==1:
-                        count+=1
+                    seen.add((i, j))
+
+        for r, c in seen:
+            if row[r]==1 and col[c] == 1:
+                count+=1 
+        # for i in range(len(mat)):
+        #     for j in range(len(mat[0])):
+        #         if mat[i][j] == 1:
+        #             if row[i]==1 and col[j]==1:
+        #                 count+=1
         return count
