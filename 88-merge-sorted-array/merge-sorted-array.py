ebaa6817a4c927e195de89f6891478e02a1d644a
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-01-05 05:42:34 -0800

    Time: 35 ms (94.48%) | Memory: 17.3 MB (13.05%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,17 +3,33 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        for i in range(len(nums1)-1, -1, -1):
-            if m<=0:
-                nums1[i] = nums2[n-1]
-                n-=1
-            elif n<=0:
-                nums1[i] = nums1[m-1]
-                m-=1
-            elif nums1[m-1]>=nums2[n-1]:
-                nums1[i] = nums1[m-1]
-                m-=1
+        # for i in range(len(nums1)-1, -1, -1):
+        #     if m<=0:
+        #         nums1[i] = nums2[n-1]
+        #         n-=1
+        #     elif n<=0:
+        #         nums1[i] = nums1[m-1]
+        #         m-=1
+        #     elif nums1[m-1]>=nums2[n-1]:
+        #         nums1[i] = nums1[m-1]
+        #         m-=1
+        #     else:
+        #         nums1[i] = nums2[n-1]
+        #         n-=1
+
+
+        i , j, k = m-1, n-1, m + n -1
+        while i>=0 and j>=0:
+            if nums1[i]>nums2[j]:
+                nums1[k] = nums1[i]
+                i-=1
             else:
-                nums1[i] = nums2[n-1]
-                n-=1
+                nums1[k] = nums2[j]
+                j-=1
+            k-=1
+        
+        while j>=0:
+            nums1[k] = nums2[j]
+            j-=1
+            k-=1
         
