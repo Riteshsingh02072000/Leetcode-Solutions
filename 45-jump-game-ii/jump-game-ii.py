3d2dd79859fe44b4bc695fdf96ec4ef8255ff3ab
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-01-09 09:49:44 -0800

    Time: 4955 ms (14.46%) | Memory: 18.5 MB (14.89%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [float('inf')]*n
+        dp[0] = 0
+
+        for i in range(n):
+            for j in range(nums[i]):
+                if i+j+1<n:
+                    dp[i+j+1] = min(dp[i+j+1], dp[i]+1)
+        return dp[-1]
