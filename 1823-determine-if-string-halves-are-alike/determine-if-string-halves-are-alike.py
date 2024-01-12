fd70f22872f1775aefd64378bf4a6608d25f6c5b
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-01-12 10:04:56 -0800

    Time: 33 ms (94.64%) | Memory: 17.3 MB (33.46%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.py
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.py
@@ -1,25 +1,25 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        # vowels = "aeiouAEIOU"
-        # c1, c2 = 0, 0
-        # n = len(s)
-        # for i in range(n):
-        #     if s[i] in vowels: 
-        #         if i<n//2:
-        #             c1+=1
-        #         else:
-        #             c2+=1
-        # return c1==c2
+        vowels = "aeiouAEIOU"
+        count = 0
+        n = len(s)
+        for i in range(n):
+            if s[i] in vowels: 
+                if i<n//2:
+                    count+=1
+                else:
+                    count-=1
+        return count==0
 
 
-        memo = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-        left, right = 0, 0
+        # memo = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        # left, right = 0, 0
 
-        hal_l = len(s) // 2
+        # hal_l = len(s) // 2
 
-        for l, r in zip(s[:hal_l], s[hal_l:]):
-            if l in memo: left += 1
-            if r in memo: right += 1
+        # for l, r in zip(s[:hal_l], s[hal_l:]):
+        #     if l in memo: left += 1
+        #     if r in memo: right += 1
 
-        return left == right
+        # return left == right
         
