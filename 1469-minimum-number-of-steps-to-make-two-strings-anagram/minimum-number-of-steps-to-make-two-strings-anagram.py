149c7772e76665798b057602cd045307e765cc51
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-01-13 11:19:11 -0800

    Time: 182 ms (47.35%) | Memory: 17.9 MB (21.41%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,18 +1,29 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        dicts = collections.defaultdict(int)
-        dictt = collections.defaultdict(int)
+        # dicts = collections.defaultdict(int)
+        # dictt = collections.defaultdict(int)
+        # ans = 0
+        
+        # for x in s:
+        #     dicts[x]+=1
+        
+        # for y in t:
+        #     dictt[y] +=1
+        
+        # for key, val in dicts.items():
+        #     if dicts[key]>dictt[key]:
+        #         ans+= (val-dictt[key])
+        # return ans
+
+        count = [0]*26
         ans = 0
+        for i in range(len(s)):
+            count[ord(t[i]) - ord('a')] +=1
+            count[ord(s[i]) - ord('a')] -=1
         
-        for x in s:
-            dicts[x]+=1
-        
-        for y in t:
-            dictt[y] +=1
-        
-        for key, val in dicts.items():
-            if dicts[key]>dictt[key]:
-                ans+= (val-dictt[key])
+        for i in range(26):
+            ans += max(0, count[i])
         return ans
+
 
 
