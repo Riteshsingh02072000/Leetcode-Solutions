178f339bcb844980cc0ad002d5f8394980257d28
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-01-15 05:17:28 -0800

    Time: 1435 ms (80.04%) | Memory: 72 MB (35.64%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -1,23 +1,20 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
         outgoing = collections.defaultdict(int)
-        seen = set()
+        # seen = set()
         for x, y in matches:
-            outgoing[y] +=1
-            seen.add(x)
-            seen.add(y)
-        print(seen)
-        ans = [[], []]
+            outgoing[x] += 0
+            outgoing[y] += 1
+            # seen.add(x)
+            # seen.add(y)
+        # print(seen)
+        # ans = [[], []]
         
-        for x in list(seen):
-            if x not in outgoing:
-                ans[0].append(x)
-            else:
-                if outgoing[x]==1:
-                    ans[1].append(x)
+        zero = sorted([x for x, v in outgoing.items() if v==0])
+        ones = sorted([x for x, v in outgoing.items() if v==1])
 
-        ans[0].sort()
-        ans[1].sort()
-        return ans
+        # ans[0].sort()
+        # ans[1].sort()
+        return [zero, ones]
         # for key, val in outgoing.values():
         #     if
