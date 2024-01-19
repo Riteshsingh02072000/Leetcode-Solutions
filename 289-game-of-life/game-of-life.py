92b2d811b4bd819ec96689dcd11469ac0e8a3f7d
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-01-19 14:24:21 -0800

    Time: 36 ms (79.29%) | Memory: 16.6 MB (56.03%) - LeetSync

--- 289-game-of-life/game-of-life.py
+++ 289-game-of-life/game-of-life.py
@@ -4,28 +4,38 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         
+
         m , n = len(board), len(board[0])
-        def count(r,c):
-            val = (
-                (board[r+1][c] if r<m-1 else 0 )+
-                (board[r-1][c] if r>0 else 0)+
-                (board[r-1][c-1] if r>0 and c>0 else 0)+
-                (board[r-1][c+1] if r>0 and c<n-1 else 0)+
-                (board[r][c-1] if c>0 else 0 )+
-                (board[r][c+1] if c<n-1 else 0) +
-                (board[r+1][c-1] if r<m-1 and c>0 else 0 )+ 
-                (board[r+1][c+1] if r<m-1 and c<n-1 else 0)
-            )
-            return val
-        
+        def inbound(r,c):
+            return 0<=r<m and 0<=c<n
+        # def count(r,c):
+        #     val = (
+        #         (board[r+1][c] if r<m-1 else 0 )+
+        #         (board[r-1][c] if r>0 else 0)+
+        #         (board[r-1][c-1] if r>0 and c>0 else 0)+
+        #         (board[r-1][c+1] if r>0 and c<n-1 else 0)+
+        #         (board[r][c-1] if c>0 else 0 )+
+        #         (board[r][c+1] if c<n-1 else 0) +
+        #         (board[r+1][c-1] if r<m-1 and c>0 else 0 )+ 
+        #         (board[r+1][c+1] if r<m-1 and c<n-1 else 0)
+        #     )
+        #     return val
+        directions = set((x,y) for x in (-1,0,1) for y in (-1,0,1) if x or y)
+        print(directions)
         tmp = collections.defaultdict(int)
         for r in range(m):
             for c in range(n):
-                ngbr = count(r,c)
-                if ngbr<2 or ngbr>3:
+                count = 0
+                for x,y in directions:
+                    nr = r+x
+                    nc = c+y
+                    if inbound(nr, nc):
+                        count += board[nr][nc]
+                # ngbr = count(r,c)
+                if count<2 or count>3:
                     if board[r][c] ==1:
                         tmp[(r,c)] = 0
-                if ngbr==3 and board[r][c]==0:
+                if count==3 and board[r][c]==0:
                     tmp[(r,c)] = 1
         
         for key, val in tmp.items():
