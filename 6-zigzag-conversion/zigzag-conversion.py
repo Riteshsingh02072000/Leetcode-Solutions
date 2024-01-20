349afa04bce338797a8c1f0f2c07ce2f247574e3
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-01-20 06:25:31 -0800

    Time: 46 ms (92.19%) | Memory: 17 MB (55.52%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,53 +1,18 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1 or numRows>=len(s):
+        if numRows == 1:
             return s
-
-        rows = [[] for row in range(numRows)]
-        steps = 1
-        index = 0
+        ans = [[] for _ in range(numRows)]
+        step = 1
+        idx = 0
         for x in s:
-            rows[index].append(x)
-            if index == 0:
-                steps = 1
-
-            if index == numRows-1:
-                steps = -1
-
-            index+=steps
-
-        for i in range(numRows):
-            rows[i] = ''.join(rows[i])
-
-        return ''.join(rows)        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # if numRows == 1 or numRows >= len(s):
-        #     return s
-        
-        # rows = [ [] for row in range(numRows)]
-        # index = 0
-        # step = -1
-        # for char in s:
-        #     rows[index].append(char)
-        #     if index==0:
-        #         step = 1
-        #     elif index == numRows-1:
-        #         step = -1
-        #     index += step
-        # print(rows)
-        # for i in range(numRows):
-        #     rows[i] = ''.join(rows[i])
-        # return ''.join(rows)
+            ans[idx].append(x)
+            if idx==0:
+                step = 1
+            elif idx == numRows-1:
+                step = -1
+            idx+=step
+        print(ans)
+        ans = ["".join(x) for x in ans]
+        return "".join(ans)
+        # return "".join("".join(x for x in ans))
