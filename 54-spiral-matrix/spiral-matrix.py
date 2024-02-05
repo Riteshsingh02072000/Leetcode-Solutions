b3a650728c7049e1098e3ab8dacd3f9ecd42b91f
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-02-05 10:24:22 -0500

    Time: 30 ms (91.38%) | Memory: 16.6 MB (69.60%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,29 +1,26 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         ans = []
-        rtop, cleft = 0,0
-        rbot = len(matrix)
-        cright = len(matrix[0])
+        cleft, rtop = 0,0
+        cright, rbottom = len(matrix[0]), len(matrix)
 
-        while rtop<rbot and cleft<cright:
+        while rtop<rbottom and cleft<cright:
             for i in range(cleft, cright):
                 ans.append(matrix[rtop][i])
             rtop+=1
 
-
-            for i in range(rtop, rbot):
+            for i in range(rtop, rbottom):
                 ans.append(matrix[i][cright-1])
-            cright-=1
-            if not (rtop<rbot and cleft<cright):
+            cright -= 1
+
+            if not (rtop<rbottom and cleft<cright):
                 break
+            
+            for i in range(cright-1, cleft-1, -1):
+                ans.append(matrix[rbottom-1][i])
+            rbottom -= 1
 
-
-            for i in range(cright-1, cleft-1,-1):
-                ans.append(matrix[rbot-1][i])
-            rbot-=1
-
-
-            for i in range(rbot-1, rtop-1, -1):
+            for i in range(rbottom-1, rtop-1, -1):
                 ans.append(matrix[i][cleft])
             cleft+=1
         return ans
