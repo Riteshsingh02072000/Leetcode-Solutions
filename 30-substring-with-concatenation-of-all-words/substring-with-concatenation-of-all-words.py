4f5499e983b32ab38347a85ab091700dc9332f58
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-02-05 08:49:29 -0500

    Time: 124 ms (70.13%) | Memory: 17.2 MB (74.61%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -2,38 +2,37 @@ class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         if not words or not s:
             return []
-        
-        word_length = len(words[0])
-        total_length = len(words)*word_length
 
-        word_count = collections.defaultdict(int)
+        word_len = len(words[0])
+        word_count = defaultdict(int)
+
         for word in words:
             word_count[word] += 1
         
         ans = []
 
-        for i in range(word_length):
+        for i in range(word_len):
+            window = defaultdict(int)
             left = i
             count = 0
-            window = collections.defaultdict(int)
 
-            for j in range(i, len(s) - word_length+1, word_length):
-                word = s[j:j+word_length]
+            for j in range(i, len(s)-word_len+1, word_len):
+                word = s[j:j+word_len]
 
                 if word in words:
-                    window[word]+=1
-                    count+=1
+                    window[word] +=1
+                    count +=1
 
                     while window[word]>word_count[word]:
-                        left_word = s[left:left+word_length]
-                        window[left_word] -=1
-                        left += word_length
-                        count-=1
+                        left_word = s[left:left+word_len]
+                        window[left_word] -= 1
+                        count -= 1
+                        left+= word_len
                     
-                    if count == len(words):
+                    if count==len(words):
                         ans.append(left)
                 else:
                     count = 0
                     window.clear()
-                    left = j+word_length
+                    left = j + word_len
         return ans
