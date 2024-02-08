fc9995b6a385fb2fc975003f541ba628208423bc
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-02-08 13:41:10 -0500

    Time: 98 ms (87.19%) | Memory: 17.5 MB (61.59%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,16 +3,27 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        seenR = set()
-        seenC = set()
+        # seenR = set()
+        # seenC = set()
+        # for i in range(len(matrix)):
+        #     for j in range(len(matrix[0])):
+        #         if matrix[i][j] == 0:
+        #             seenR.add(i)
+        #             seenC.add(j)
+        
+        # for i in range(len(matrix)):
+        #     for j in range(len(matrix[0])):
+        #         if i in seenR or j in seenC:
+        #             matrix[i][j] = 0
+
+        valid = []
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    seenR.add(i)
-                    seenC.add(j)
-        
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if i in seenR or j in seenC:
-                    matrix[i][j] = 0
+                    valid.append((i,j))
+        for row, col in valid:
+            matrix[row] = [0]*len(matrix[0])
+
+            for i in range(len(matrix)):
+                matrix[i][col] = 0
 
