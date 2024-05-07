7330b31a885024776c8fb475e7f936ccfd502ecb
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-05-07 10:40:45 +0530

    Time: 249 ms (52.42%) | Memory: 19.3 MB (64.32%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
@@ -3,18 +3,32 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-import sys
-sys.set_int_max_str_digits(0)
+# import sys
+# sys.set_int_max_str_digits(0)
 class Solution:
     def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        num = ""
-        while head:
-            num+=str(head.val)
-            head = head.next
+        reversed_list = self.reversed(head)
+        cur = reversed_list
+        prev = None
+        carry = 0
+
+        while cur:
+            new_val = cur.val*2 + carry
+            cur.val = new_val%10
+            carry = 1 if new_val>9 else 0
+            prev, cur = cur, cur.next
         
-        ans = str(2*int(num))
-        dummy = tmp = ListNode(0)
-        for i in range(len(ans)):
-            tmp.next = ListNode(int(ans[i]))
-            tmp = tmp.next
-        return dummy.next
+        if carry:
+            prev.next = ListNode(carry)
+        return self.reversed(reversed_list)
+    
+    def reversed(self, node):
+        cur = node
+        prev = None
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+        return prev
+        
