9ca73b85ca48fcad4d1041243a88e2adfa9b9157
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-06-05 08:31:17 +0530

    Time: 565 ms (69.45%) | Memory: 23 MB (68.42%) - LeetSync

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
@@ -0,0 +1,21 @@
+class Solution:
+    def clearStars(self, s: str) -> str:
+        n = len(s)
+        bucket = [[] for i in range(26)]
+        removed =[False]*n
+
+        for i in range(n):
+            if s[i] == '*':
+                removed[i] = True
+                for j in range(26):
+                    if bucket[j]:
+                        removed[bucket[j].pop()] = True
+                        break
+            
+            else:
+                bucket[ord(s[i])-ord('a')].append(i)
+        ans = []
+        for i in range(n):
+            if not removed[i]:
+                ans.append(s[i])
+        return ''.join(ans)
