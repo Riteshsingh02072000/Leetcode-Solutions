b07a80b269a146a880da6f4454f30c1f65d83616
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-06-25 08:40:57 +0530

    Time: 47 ms (5.60%) | Memory: 16.5 MB (74.62%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.py
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.py
@@ -6,22 +6,35 @@
 #         self.right = right
 class Solution:
     def bstToGst(self, root: TreeNode) -> TreeNode:
-        trav = []
+        # trav = []
+        # def dfs(node):
+        #     if not node:
+        #         return
+        #     dfs(node.left)
+        #     trav.append(node.val)
+        #     dfs(node.right)
+        # dfs(root)
+        
+        # def traverse(node):
+        #     if not node:
+        #         return
+        #     idx = trav.index(node.val)
+        #     node.val = sum(trav[idx:])
+
+        #     traverse(node.left)
+        #     traverse(node.right)
+        # traverse(root)
+        # return root
+
+        self.val = 0 
+
         def dfs(node):
             if not node:
                 return
-            dfs(node.left)
-            trav.append(node.val)
+            
             dfs(node.right)
+            self.val+=node.val
+            node.val = self.val
+            dfs(node.left)
         dfs(root)
-        
-        def traverse(node):
-            if not node:
-                return
-            idx = trav.index(node.val)
-            node.val = sum(trav[idx:])
-
-            traverse(node.left)
-            traverse(node.right)
-        traverse(root)
         return root
