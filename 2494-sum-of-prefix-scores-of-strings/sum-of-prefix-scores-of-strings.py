f630774bc57681f7e9ce02eb91e547fdc445cdd7
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-09-25 08:21:29 +0530

    Time: 1165 ms (97.24%) | Memory: 211.6 MB (96.90%) - LeetSync

--- 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.py
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.py
@@ -1,19 +1,43 @@
 class Solution:
     def sumPrefixScores(self, words: List[str]) -> List[int]:
-        d = defaultdict(int)
-        for x in words:
-            s = ""
-            for y in x:
-                s+=y
-                d[s]+=1
+        # d = defaultdict(int)
+        # for x in words:
+        #     s = ""
+        #     for y in x:
+        #         s+=y
+        #         d[s]+=1
         
-        ans = []
+        # ans = []
 
+        # for word in words:
+        #     score = 0
+        #     s = ""
+        #     for x in word:
+        #         s+=x
+        #         score+=d[s]
+        #     ans.append(score)
+        # return ans
+
+        trie = self.buildTrie(words)
+        return self.helper(trie, words)
+
+    def buildTrie(self, words):
+        trie ={}
         for word in words:
+            node = trie
+            for char in word:
+                node = node.setdefault(char, {})
+
+                node['$'] = node.get('$', 0) +1
+        return trie
+    
+    def helper(self, trie, words):
+        ans = []
+        for word in words:
+            node = trie
             score = 0
-            s = ""
-            for x in word:
-                s+=x
-                score+=d[s]
+            for char in word:
+                node = node[char]
+                score+=node['$']
             ans.append(score)
         return ans
