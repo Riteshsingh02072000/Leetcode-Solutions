f5a23387b6193829d33e4c49f3114d226c6b4b80
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-10-05 12:57:02 +0530

    Time: 77 ms (43.51%) | Memory: 16.8 MB (22.10%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,46 +1,53 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1)>len(s2):
-            return False
-        
-        s1count, s2count = [0]*26, [0]*26
-        for i in range(len(s1)):
-            s1count[ord(s1[i]) - ord('a')]+=1
-            s2count[ord(s2[i]) - ord('a')] +=1
-        
-        matches = 0
-        for i in range(26):
-            matches += (1 if s1count[i]==s2count[i] else 0)
-        
-        l = 0
-        for r in range(len(s1), len(s2)):
-            if matches == 26:
-                return True
-            index = ord(s2[r])-ord('a')
-            s2count[index]+=1
-            if s1count[index] == s2count[index]:
-                matches+=1
-            elif s1count[index]+1 == s2count[index]:
-                matches-=1
-            
-            index = ord(s2[l])-ord('a')
-            s2count[index]-=1
-            if s1count[index] == s2count[index]:
-                matches+=1
-            elif s1count[index]-1 == s2count[index]:
-                matches-=1
-            l+=1
-        return matches == 26
-
-        # count1 =  collections.Counter(s1)
-        # l1 = len(s1)
-        # l2 = len(s2)
-        # if l1>l2:
+        # if len(s1)>len(s2):
         #     return False
         
-        # for i in range(l2-l1+1):
-        #     count2 = collections.Counter(s2[i:i+l1])
-        #     if count1== count2:
-        #         return True
+        # s1count, s2count = [0]*26, [0]*26
+        # for i in range(len(s1)):
+        #     s1count[ord(s1[i]) - ord('a')]+=1
+        #     s2count[ord(s2[i]) - ord('a')] +=1
         
-        # return False
+        # matches = 0
+        # for i in range(26):
+        #     matches += (1 if s1count[i]==s2count[i] else 0)
+        
+        # l = 0
+        # for r in range(len(s1), len(s2)):
+        #     if matches == 26:
+        #         return True
+        #     index = ord(s2[r])-ord('a')
+        #     s2count[index]+=1
+        #     if s1count[index] == s2count[index]:
+        #         matches+=1
+        #     elif s1count[index]+1 == s2count[index]:
+        #         matches-=1
+            
+        #     index = ord(s2[l])-ord('a')
+        #     s2count[index]-=1
+        #     if s1count[index] == s2count[index]:
+        #         matches+=1
+        #     elif s1count[index]-1 == s2count[index]:
+        #         matches-=1
+        #     l+=1
+        # return matches == 26
+
+        count1 =  collections.Counter(s1)
+        l1 = len(s1)
+        l2 = len(s2)
+        if l1>l2:
+            return False
+        count2 = Counter(s2[0:l1])
+        if count1==count2:
+            return True
+
+        l = 1
+        r = l1
+        while r<l2:
+            count2[s2[l-1]] -= 1
+            count2[s2[r]] += 1
+            if count1 == count2:
+                return True
+            l+=1
+            r+=1
+        return False
