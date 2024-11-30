fb83bab3f456ee50440b7b6c32e9c7657b4678c0
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2024-11-30 09:30:10 +0530

    Time: 12 ms (24.91%) | Memory: 18.9 MB (6.40%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,21 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # dct = collections.defaultdict(int)
-        # for x in nums:
-        #     dct[x]+=1
-        #     if dct[x] >= len(nums)/2:
-        #         return x
-
-        count = 0 
-        ans = 0
+        dct= defaultdict(int)
+        n = len(nums)
 
         for x in nums:
-            if count == 0:
-                ans = x
-            
-            if x == ans:
-                count+=1
-            
-            else:
-                count -=1
-        return ans
+            dct[x] += 1
+            if dct[x] > n//2:
+                return x
+        return -1
