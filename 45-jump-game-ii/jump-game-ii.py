ed32839219e882ee6592c1c8819427d242dd8c9d
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-01-04 12:11:15 +0530

    Time: 5053 ms (9.16%) | Memory: 18.8 MB (15.25%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -7,5 +7,5 @@ class Solution:
         for i in range(n):
             for j in range(nums[i]):
                 if i+j+1<n:
-                    dp[i+j+1] = min(dp[i+j+1], dp[i]+1)
+                    dp[i+j+1]  = min(dp[i+j+1], 1+dp[i])
         return dp[-1]
