21c48bfff6b69c86ef6ebeac3ab3dff052a5f0a9
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-01-09 04:24:14 +0530

    Time: 511 ms (54.39%) | Memory: 104.9 MB (46.59%) - LeetSync

--- 3678-design-task-manager/design-task-manager.py
+++ 3678-design-task-manager/design-task-manager.py
@@ -1,24 +1,23 @@
 class TaskManager:
 
     def __init__(self, tasks: List[List[int]]):
-        self.pheap = []
+        self.heap = []
         self.tmap = {}
-        
+
         for task in tasks:
-            uid, tid, p = tuple(task)
-            self.add(uid, tid, p)
-        
+            u, t, p = task
+            self.add(u, t, p)
 
     def add(self, userId: int, taskId: int, priority: int) -> None:
-
-        heapq.heappush(self.pheap, (-priority, -taskId, userId))
-        self.tmap[taskId] = (priority, userId)
+        heapq.heappush(self.heap, (-priority, -taskId, userId))
+        # print(self.heap)
+        self.tmap[taskId] = (userId, priority)
         
 
     def edit(self, taskId: int, newPriority: int) -> None:
-        oldPriority, uid = self.tmap[taskId]
-        self.tmap[taskId] = (newPriority, uid)
-        heappush(self.pheap, (-newPriority, -taskId, uid))
+        u, p = self.tmap[taskId]
+        self.tmap[taskId] = None
+        self.add(u, taskId, newPriority)
         
 
     def rmv(self, taskId: int) -> None:
@@ -26,18 +25,58 @@ class TaskManager:
         
 
     def execTop(self) -> int:
+        # print(self.heap)
+        if not self.heap:
+            return -1
+        
+        p, t, u = heapq.heappop(self.heap)
+        # print(-t in self.tmap)
+        if self.tmap[-t] and self.tmap[-t] == (u, -p):
+            # print('x')
+            self.tmap[-t] = None
+            # print(u)
+            return u
+        return self.execTop()
+    
+    
+    # def __init__(self, tasks: List[List[int]]):
+    #     self.pheap = []
+    #     self.tmap = {}
+        
+    #     for task in tasks:
+    #         uid, tid, p = tuple(task)
+    #         self.add(uid, tid, p)
+        
 
-        #print(self.pheap)
+    # def add(self, userId: int, taskId: int, priority: int) -> None:
 
-        while self.pheap:
-            neg_priority, neg_taskId, userId = heappop(self.pheap)
-            priority, taskId = -neg_priority, -neg_taskId
+    #     heapq.heappush(self.pheap, (-priority, -taskId, userId))
+    #     self.tmap[taskId] = (priority, userId)
+        
 
-            if taskId in self.tmap and self.tmap[taskId] == (priority, userId):
-                del self.tmap[taskId]
+    # def edit(self, taskId: int, newPriority: int) -> None:
+    #     oldPriority, uid = self.tmap[taskId]
+    #     self.tmap[taskId] = (newPriority, uid)
+    #     heappush(self.pheap, (-newPriority, -taskId, uid))
+        
 
-                return userId
-        return -1
+    # def rmv(self, taskId: int) -> None:
+    #     self.tmap[taskId] = None
+        
+
+    # def execTop(self) -> int:
+
+    #     #print(self.pheap)
+
+    #     while self.pheap:
+    #         neg_priority, neg_taskId, userId = heappop(self.pheap)
+    #         priority, taskId = -neg_priority, -neg_taskId
+
+    #         if taskId in self.tmap and self.tmap[taskId] == (priority, userId):
+    #             del self.tmap[taskId]
+
+    #             return userId
+    #     return -1
 
         # if not self.pheap: 
         #     return -1
