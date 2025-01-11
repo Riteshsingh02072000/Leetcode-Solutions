9de8044245f75684e715ab794c77e55116120e2e
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-01-11 22:59:23 +0530

    Time: 19 ms (38.61%) | Memory: 20.7 MB (50.47%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,16 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anag = defaultdict(list)
-        ans = []
-
-        for word in strs:
-            word_list = list(word)
-            word_list.sort()
-
-            cur = ''.join(word_list)
-            anag[cur].append(word)
         
-        for val in anag.values():
-            ans.append(val)
-
+        for x in strs:
+            word = list(x)
+            word.sort()
+            print(word)
+            cur = ''.join(word)
+            anag[cur].append(x)
+        ans = []
+        for x in anag.values():
+            ans.append(x)
         return ans
