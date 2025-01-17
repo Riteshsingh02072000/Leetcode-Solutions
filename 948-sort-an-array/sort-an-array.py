90f28ed98f5599b83dd2bd5576e6cb5b6750b192
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-01-17 07:16:16 +0530

    Time: 659 ms (49.85%) | Memory: 24.2 MB (78.37%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.py
@@ -0,0 +1,41 @@
+class Solution:
+    def mergeSort(self, array):
+        n = len(array)
+        if n<=1:
+            return array
+        mid = n//2
+
+        left = array[:mid]
+        right = array[mid:]
+        self.mergeSort(left)
+        self.mergeSort(right)
+
+        self.merge(left, right, array)
+        return array
+    
+    def merge(self, arr1, arr2, source):
+        p1, p2 = 0,0
+        n1, n2 = len(arr1), len(arr2)
+
+        i = 0
+        while p1<n1 or p2<n2:
+            if p1 == n1 or (p2!=n2 and arr2[p2]<arr1[p1]):
+                source[i] = arr2[p2]
+                p2+=1
+            elif p2 == n2 or (arr1[p1]<=arr2[p2]):
+                source[i] = arr1[p1]
+                p1+=1
+            i+=1
+        
+
+
+
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return self.mergeSort(nums)
+        # return nums
+        # heap = nums[:]
+        # heapq.heapify(heap)
+        # for i in range(len(nums)):
+        #     nums[i] = heapq.heappop(heap)
+
+        # return nums
