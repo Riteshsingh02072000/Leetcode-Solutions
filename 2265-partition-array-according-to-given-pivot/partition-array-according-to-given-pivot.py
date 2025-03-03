ffe15df4b9f3baec3c64127778844afad84cef64
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-03-03 08:14:35 +0530

    Time: 56 ms (20.57%) | Memory: 34.8 MB (25.49%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,14 +1,30 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        prefix = []
-        suffix = []
-        same = 0
+        # prefix = []
+        # suffix = []
+        # same = 0
 
-        for x in nums:
-            if x<pivot:
-                prefix.append(x)
-            elif x>pivot:
-                suffix.append(x)
-            else:
-                same+=1
-        return prefix + [pivot]*same + suffix
+        # for x in nums:
+        #     if x<pivot:
+        #         prefix.append(x)
+        #     elif x>pivot:
+        #         suffix.append(x)
+        #     else:
+        #         same+=1
+        # return prefix + [pivot]*same + suffix
+        n = len(nums)
+        ans = [0]*n
+        l, r = 0, n-1
+
+        for i, j in zip(range(len(nums)), range(len(nums)-1, -1, -1)):
+            if nums[i]<pivot:
+                ans[l] = nums[i]
+                l += 1
+            if nums[j] > pivot:
+                ans[r] = nums[j]
+                r-=1
+        
+        while l<=r:
+            ans[l] = pivot
+            l+=1
+        return ans
