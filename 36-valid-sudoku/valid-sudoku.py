56ed2214f3ee2737310b62e0ebe9b5d8bb875b5e
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-03-07 08:48:08 +0530

    Time: 7 ms (38.76%) | Memory: 17.7 MB (69.71%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,28 +1,14 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row = collections.defaultdict(set)
-        col = collections.defaultdict(set)
-        square = collections.defaultdict(set)
-
-
-        for i in range(9):
-            for j in range(9):
-                cur = board[i][j]
-                if cur == '.':
-                    continue
-                if cur in row[i] or cur in col[j] or cur in square[(i//3, j//3)]:
-                    return False
-                row[i].add(cur)
-                col[j].add(cur)
-                square[(i//3, j//3)].add(cur)
-        return True
-
+        # row = collections.defaultdict(set)
+        # col = collections.defaultdict(set)
+        # square = collections.defaultdict(set)
 
 
         # for i in range(9):
         #     for j in range(9):
         #         cur = board[i][j]
-        #         if cur == ".":
+        #         if cur == '.':
         #             continue
         #         if cur in row[i] or cur in col[j] or cur in square[(i//3, j//3)]:
         #             return False
@@ -30,3 +16,22 @@ class Solution:
         #         col[j].add(cur)
         #         square[(i//3, j//3)].add(cur)
         # return True
+
+
+
+        row = collections.defaultdict(set)
+        col = collections.defaultdict(set)
+        square = collections.defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                cur = board[i][j]
+                if cur == ".":
+                    continue
+                
+                if cur in row[i] or cur in col[j] or cur in square[(i//3, j//3)]:
+                    return False
+                row[i].add(cur)
+                col[j].add(cur)
+                square[(i//3, j//3)].add(cur)
+        return True     
