b65dd426194040879cda67d02582332ef1df0677
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-03-26 20:12:51 +0530

    Time: 288 ms (20.84%) | Memory: 29.1 MB (5.92%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,58 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # n = len(grid)
-        # m = len(grid[0])
-        # count = 0
-        # direction = [0,1,0,-1,0]
-        # # visited = set()
-
-        # def inBound(i, j):
-        #     # print(visited)
-        #     return 0<=i<n and 0<=j<m
-        
-        # def trav(i, j):
-        #     if not inBound(i, j):
-        #         return
-        #     if grid[i][j]!='1':
-        #         return
-        #     grid[i][j] = 'V'
-        #     for x in range(4):
-        #         nr = i+direction[x]
-        #         nc = j + direction[x+1]
-        #         trav(nr, nc)
-
-        
-        # for i in range(n):
-        #     for j in range(m):
-        #         if grid[i][j]=='1':
-        #             trav(i, j)
-        #             count+=1
-        # return count
-
-
         n = len(grid)
         m = len(grid[0])
-        count = 0
+        visited = set()
+        ans = 0
         direction = [0, 1, 0, -1, 0]
 
         def inBound(i, j):
             return 0<=i<n and 0<=j<m
         
-        def trav(i, j):
-            if not inBound(i, j):
-                return
-            if grid[i][j] != '1':
+        def dfs(i, j):
+            # if not inBound(i, j):
+            #     return 
+            if (i, j) in visited:
                 return
             
-            grid[i][j]= 'V'
+            visited.add((i, j))
             for x in range(4):
                 nr = i + direction[x]
                 nc = j + direction[x+1]
-                trav(nr, nc)
-        
+
+                if inBound(nr, nc) and grid[nr][nc] == '1':
+                    dfs(nr, nc)
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == '1':
-                    trav(i, j)
-                    count+=1
-        return count
+                if grid[i][j] == '1' and (i, j) not in visited:
+                    dfs(i, j)
+                    ans += 1
+        return ans
