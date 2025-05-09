be62c5574ad5575e127a04fbda3c725322416773
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-05-09 13:32:26 +0530

    Time: 11 ms (5.76%) | Memory: 18.2 MB (6.52%) - LeetSync

---
+++ 1751-slowest-key/slowest-key.py
@@ -0,0 +1,27 @@
+class Solution:
+    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
+        # ans = keysPressed[0]
+        # n = len(releaseTimes)
+        # mx = releaseTimes[0]
+        # for i in range(1, n):
+        #     if (releaseTimes[i]-releaseTimes[i-1])>=mx:
+        #         if mx == releaseTimes[i]-releaseTimes[i-1]:
+        #             ans = chr(max(ord(ans), ord(keysPressed[i])))
+        #         else:
+        #             mx = releaseTimes[i]-releaseTimes[i-1]
+        #             ans = keysPressed[i]
+        # return ans
+
+        max_heap = [(-releaseTimes[0],-ord(keysPressed[0]), keysPressed[0])]
+        # curr_time 
+        for i in range(1,len(releaseTimes)):
+            curr_time = releaseTimes[i] - releaseTimes[i-1]
+            # Flip the second key by negating the ASCII value of the key
+            heapq.heappush(max_heap, (-curr_time, -ord(keysPressed[i]), keysPressed[i]))
+
+                           
+                        
+        print(max_heap)
+        
+        top_duration, top_key, t = heapq.heappop(max_heap)
+        return t
