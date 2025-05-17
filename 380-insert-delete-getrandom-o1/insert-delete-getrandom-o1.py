8c79d9401687971dc9e667365b90cc0740135f4c
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-05-17 07:39:24 +0530

    Time: 348 ms (7.47%) | Memory: 57.7 MB (91.50%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,27 +1,27 @@
+import random
 class RandomizedSet:
 
     def __init__(self):
-        self.check = set()
+        self.randomized = set()
         
 
     def insert(self, val: int) -> bool:
-        if val not in self.check:
-            self.check.add(val)
+        if val not in self.randomized:
+            self.randomized.add(val)
             return True
         return False
         
 
-        
-
     def remove(self, val: int) -> bool:
-        if val in self.check:
-            self.check.remove(val)
+        if val in self.randomized:
+            self.randomized.remove(val)
             return True
         return False
         
 
     def getRandom(self) -> int:
-        return random.choice(list(self.check))
+        return random.choice(list(self.randomized))
+
         
 
 
