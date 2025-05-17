b93dc7e8743e8e8c975667e7c96aa528dbe3804f
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-05-17 07:27:51 +0530

    Time: 11 ms (44.35%) | Memory: 18.7 MB (34.54%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,11 +1,23 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [float('inf')]*n
-        dp[0] = 0
+        farthest = 0
+        jump = 0 
+        curEnd = 0
 
-        for i in range(n):
-            for j in range(nums[i]):
-                if i+j+1<n:
-                    dp[i+j+1]  = min(dp[i+j+1], 1+dp[i])
-        return dp[-1]
+        for i in range(n-1):
+            farthest = max(farthest, i + nums[i])
+            if i == curEnd:
+                jump+=1
+                curEnd = farthest
+        return jump
+        
+        # n = len(nums)
+        # ans = [float('inf') for x in nums]
+        # ans[0] = 0
+
+        # for i in range(n):
+        #     for j in range(nums[i]):
+        #         if i+j+1<n:
+        #             ans[i+j+1] = min(ans[i+j+1], 1+ans[i])
+        # return ans[-1]
