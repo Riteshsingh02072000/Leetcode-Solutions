f86feeeae430e3554f0fcf965c93d02c74a4852a
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-05-17 07:52:20 +0530

    Time: 122 ms (69.11%) | Memory: 57.9 MB (64.77%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -2,25 +2,41 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        self.randomized = set()
+        # self.randomized = set()
+        self.lst = []
+        self.idxMap = {}
         
-
+    # def search(self, val):
+    #     return val in self.idx
     def insert(self, val: int) -> bool:
-        if val not in self.randomized:
-            self.randomized.add(val)
-            return True
-        return False
+        # if val not in self.randomized:
+        #     self.randomized.add(val)
+        #     return True
+        # return False
+        if val in self.idxMap:
+            return False
+        self.lst.append(val)
+        self.idxMap[val] = len(self.lst)-1
+        return True
         
 
     def remove(self, val: int) -> bool:
-        if val in self.randomized:
-            self.randomized.remove(val)
-            return True
-        return False
+        # if val in self.randomized:
+        #     self.randomized.remove(val)
+        #     return True
+        # return False
+        if val not in self.idxMap:
+            return False
+        idx = self.idxMap[val]
+        self.lst[idx] = self.lst[-1]
+        self.idxMap[self.lst[-1]] = idx
+        self.lst.pop()
+        del self.idxMap[val]
+        return True
         
 
     def getRandom(self) -> int:
-        return random.choice(list(self.randomized))
+        return random.choice(self.lst)
 
         
 
