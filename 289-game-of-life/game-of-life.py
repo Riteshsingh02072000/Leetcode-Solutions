24720a40cd601f75b6381519d16ce685f344bf57
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-05-18 18:31:14 +0530

    Time: 1 ms (31.21%) | Memory: 17.7 MB (76.05%) - LeetSync

--- 289-game-of-life/game-of-life.py
+++ 289-game-of-life/game-of-life.py
@@ -3,76 +3,28 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        m, n = len(board), len(board[0])
+        tmp = defaultdict(int)
+
+        def inBound(i, j):
+            return 0<=i<m and 0<=j<n
         
-
-        # m , n = len(board), len(board[0])
-        # def inbound(r,c):
-        #     return 0<=r<m and 0<=c<n
-        # # def count(r,c):
-        # #     val = (
-        # #         (board[r+1][c] if r<m-1 else 0 )+
-        # #         (board[r-1][c] if r>0 else 0)+
-        # #         (board[r-1][c-1] if r>0 and c>0 else 0)+
-        # #         (board[r-1][c+1] if r>0 and c<n-1 else 0)+
-        # #         (board[r][c-1] if c>0 else 0 )+
-        # #         (board[r][c+1] if c<n-1 else 0) +
-        # #         (board[r+1][c-1] if r<m-1 and c>0 else 0 )+ 
-        # #         (board[r+1][c+1] if r<m-1 and c<n-1 else 0)
-        # #     )
-        # #     return val
-        # x = product([-1, 0, 1], [-1, 0, 1])
-        # for r, c in product([-1, 0,1], [-1, 0, 1]):
-        #     print((r,c))
-        # # print(x)
-        # directions = set((x,y) for x in (-1,0,1) for y in (-1,0,1) if x or y)
-        # print(directions)
-        # tmp = collections.defaultdict(int)
-        # for r in range(m):
-        #     for c in range(n):
-        #         count = 0
-        #         for x,y in directions:
-        #             nr = r+x
-        #             nc = c+y
-        #             if inbound(nr, nc):
-        #                 count += board[nr][nc]
-        #         # ngbr = count(r,c)
-        #         if count<2 or count>3:
-        #             if board[r][c] ==1:
-        #                 tmp[(r,c)] = 0
-        #         if count==3 and board[r][c]==0:
-        #             tmp[(r,c)] = 1
-        
-        # for key, val in tmp.items():
-        #     x, y = key
-        #     board[x][y] = val
-        # return board
-
-
-        row, col = len(board), len(board[0])
-        tmp = collections.defaultdict(int)
-
-        def inBound(r, c):
-            return 0<=r<row and 0<=c<col
-        
-        for r in range(row):
-            for c in range(col):
+        for i in range(m):
+            for j in range(n):
                 count = 0
                 for x, y in product([-1, 0, 1], [-1, 0, 1]):
-                    if x==0 and y==0:
+                    if x == 0 and y == 0:
                         continue
-                    nr = r+x
-                    nc = c+y
+                    nr = i+x
+                    nc = j+y
+
                     if inBound(nr, nc):
-                        count+=board[nr][nc]
-                if (count<2 or count>3) and board[r][c]==1:
-                    tmp[(r,c)] = 0
-                elif count == 3 and board[r][c]==0:
-                    tmp[(r,c)] = 1
+                        count += board[nr][nc]
+                if (count>3 or count<2) and board[i][j] == 1:
+                    tmp[(i, j)] = 0
+                elif count == 3 and board[i][j] == 0:
+                    tmp[(i, j)] = 1
         
-        for key, v in tmp.items():
-            r, c = key
-            board[r][c] = v
-        
-
-
-                
+        for key, val in tmp.items():
+            x, y = key
+            board[x][y] = val
