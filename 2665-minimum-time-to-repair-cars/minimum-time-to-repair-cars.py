6575f2b0392f5f5ef3669cacbc8c5bde8b75e70e
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-05-22 03:33:02 +0530

    Time: 995 ms (42.47%) | Memory: 21.9 MB (82.30%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -1,23 +1,23 @@
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
-        
-        def countcars(m):
-            car = 0
-            for x in ranks:
-                car += int(math.sqrt(m/x))
-                # car += math.floor(n)
-            return car
-        
+        if len(ranks) == 1:
+            return ranks[0]*(cars**2)
 
-        l = 1
-        r = ranks[0]*(cars*cars)
-        ans = -1
+        def countCar(m):
+            ans = 0
+            for x in ranks:
+                ans += int(math.sqrt(m/x))
+            return ans
+        
+        l, r = 1, ranks[0]*(cars**2)
+        ans = 0
         while l<=r:
             mid = (l+r)//2
-            repaired = countcars(mid)
-            if repaired>=cars:
+            repaired = countCar(mid)
+            if repaired >= cars:
                 ans = mid
-                r = mid-1
+                r = mid -1
             else:
                 l = mid+1
         return ans
+
