b8ad17fdf2cdffd53e59783b274772f1899257b4
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-05-21 20:04:37 +0530

    Time: 3 ms (36.77%) | Memory: 21.1 MB (72.45%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -16,17 +16,25 @@ class Solution:
         #     inOrder(node.right)
         # inOrder(root)
         # return ans[k-1]
-        stack = []
-        cur =root
-        n = 0
+        val = []
+        def inOrder(root):
+            if not root:
+                return
+            inOrder(root.left)
+            val.append(root.val)
+            inOrder(root.right)
+            if len(val)>=k:
+                return val[k-1]
+        inOrder(root)
+        return val[k-1]
 
-        while cur or stack:
-            while cur:
-                stack.append(cur)
-                cur = cur.left
+        # while cur or stack:
+        #     while cur:
+        #         stack.append(cur)
+        #         cur = cur.left
             
-            cur = stack.pop()
-            n+=1
-            if n==k:
-                return cur.val
-            cur = cur.right
+        #     cur = stack.pop()
+        #     n+=1
+        #     if n==k:
+        #         return cur.val
+        #     cur = cur.right
