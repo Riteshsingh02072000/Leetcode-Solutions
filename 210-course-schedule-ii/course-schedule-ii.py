2e3abed0785be219b734043425059755a329980b
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-05-24 10:27:04 +0530

    Time: 6 ms (32.45%) | Memory: 18.9 MB (95.39%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -4,23 +4,34 @@ class Solution:
         for u, v in prerequisites:
             graph[v].append(u)
         
-        indegrees = [0]*numCourses
+        indegree =[0]*numCourses
         for u, v in prerequisites:
-            indegrees[u] +=1
+            indegree[u] += 1
         
         q = deque()
         for course in range(numCourses):
-            if indegrees[course] == 0:
+            if indegree[course] == 0:
                 q.append(course)
+        
         ans = []
         while q:
             course = q.popleft()
             ans.append(course)
             if len(ans) == numCourses:
                 return ans
-            
-            for preq in graph[course]:
-                indegrees[preq]-=1
-                if indegrees[preq]==0:
-                    q.append(preq)
+            for ngbr in graph[course]:
+                indegree[ngbr] -= 1
+                if indegree[ngbr] == 0:
+                    q.append(ngbr)
         return []
+
+        
+        
+        
+        
+            
+        #     for preq in graph[course]:
+        #         indegrees[preq]-=1
+        #         if indegrees[preq]==0:
+        #             q.append(preq)
+        # return []
