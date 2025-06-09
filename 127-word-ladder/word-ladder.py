47df7db8186e628912e6c0c2f7972050ba835962
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2025-06-09 09:12:32 +0530

    Time: 55 ms (86.45%) | Memory: 21.1 MB (33.46%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -4,18 +4,43 @@ class Solution:
         if endWord not in wordList:
             return 0
         
-        q = deque([(beginWord, 1)])
-        visited = set()
+        adj = collections.defaultdict(list)
         n = len(beginWord)
-        while q:
 
-            word, step = q.popleft()
-            for i in range(n):
-                for j in range(26):
-                    newWord = word[:i] + chr(97+j) + word[i+1:]
-                    if newWord == endWord:
-                        return step+1
-                    if newWord in wordList and newWord not in visited:
-                        q.append((newWord, step+1))
-                        visited.add(newWord)
+        for word in wordList:
+            for j in range(n):
+                pattern = word[:j] + '*' + word[j+1:]
+                adj[pattern].append(word)
+        
+        visited = set([beginWord])
+        steps = 1
+        q = deque([beginWord])
+
+        while q:
+            for _ in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return steps
+                for i in range(n):
+                    pattern = word[:i] + '*' + word[i+1:]
+                    for adjWord in adj[pattern]:
+                        if adjWord not in visited:
+                            visited.add(adjWord)
+                            q.append(adjWord)
+            steps += 1
         return 0
+        # q = deque([(beginWord, 1)])
+        # visited = set()
+        # n = len(beginWord)
+        # while q:
+
+        #     word, step = q.popleft()
+        #     for i in range(n):
+        #         for j in range(26):
+        #             newWord = word[:i] + chr(97+j) + word[i+1:]
+        #             if newWord == endWord:
+        #                 return step+1
+        #             if newWord in wordList and newWord not in visited:
+        #                 q.append((newWord, step+1))
+        #                 visited.add(newWord)
+        # return 0
