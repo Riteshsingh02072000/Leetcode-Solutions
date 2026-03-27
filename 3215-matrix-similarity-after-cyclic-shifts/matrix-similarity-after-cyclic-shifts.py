091b2f57be06df8478f8a87d25ddf6d84bc3cdd3
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2026-03-27 07:12:03 +0530

    Time: 4 ms (37.65%) | Memory: 19.2 MB (98.82%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.py
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.py
@@ -2,12 +2,26 @@ class Solution:
     def areSimilar(self, mat: List[List[int]], k: int) -> bool:
         m, n = len(mat), len(mat[0])
         k = k%n
-        tmp = [row[:] for row in mat]
-        # print(mat[0][-1:], mat[0][:-1])
-        for i in range(k):
-            for j in range(m):
-                if j %2==0:
-                    mat[j] = mat[j][1:] + mat[j][:1]
-                elif j%2!=0:
-                    mat[j] = mat[j][-1:] + mat[j][:-1]
-        return tmp == mat
+
+        for i in range(m):
+            for j in range(n):
+                if i%2==0:
+                    expected = mat[i][(j+k)%n]
+                else:
+                    expected = mat[i][(j-k)%n]
+                if expected != mat[i][j]:
+                    return False
+        return True
+        
+        
+        # m, n = len(mat), len(mat[0])
+        # k = k%n
+        # tmp = [row[:] for row in mat]
+        # # print(mat[0][-1:], mat[0][:-1])
+        # for i in range(k):
+        #     for j in range(m):
+        #         if j %2==0:
+        #             mat[j] = mat[j][1:] + mat[j][:1]
+        #         elif j%2!=0:
+        #             mat[j] = mat[j][-1:] + mat[j][:-1]
+        # return tmp == mat
