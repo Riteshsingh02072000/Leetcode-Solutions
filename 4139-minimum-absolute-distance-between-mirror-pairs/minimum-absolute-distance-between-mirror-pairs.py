5c01d95ad1b8129caf1eab9348f774d565737eeb
RITESH SINGH <53328670+Riteshsingh02072000@users.noreply.github.com>
2026-04-17 07:59:33 +0530

    Time: 324 ms (29.84%) | Memory: 42 MB (51.16%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.py
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.py
@@ -1,14 +1,20 @@
 class Solution:
     def minMirrorPairDistance(self, nums: List[int]) -> int:
         def mirror(x):
-            return int(str(x)[::-1])
+            rev = 0
+            while x:
+                rev = (rev*10) + (x%10)
+                x//=10
+            return rev
 
-        mp = defaultdict(int)
+        mp = {}
         ans = float('inf')
         for i, num in enumerate(nums):
             if num in mp:
-                ans = min(ans, abs(mp[num]-i))
+                ans = min(ans, i - mp[num])
+
             mp[mirror(num)] = i
+
 
         
         return -1 if ans==float('inf') else ans
